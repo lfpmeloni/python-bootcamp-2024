4f6cf8bd7d54344ae48bcb32b4547e37c99ac56d
Felipe Meloni <felipemeloni@hotmail.com>
2024-11-07 17:35:42 +0100

    Finished project of chapter 18

--- projects/django_book/learning_logs/models.py
+++ projects/django_book/learning_logs/models.py
@@ -1,13 +1,30 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
 
 class Topic(models.Model):
 
-    text = models.charField(max_length=200)
+    text = models.CharField(max_length=200)
     data_added = models.DateTimeField(auto_now=True)
     def __str__(self):
         """
         Return a string representation of the model
         """        
         return self.text
+    
+class Entry(models.Model):
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry.
+
+        Returns:
+            str: beginning of the text
+        """
+        return f"{self.text[:50]}..."
--- projects/django_book/learning_logs/views.py
+++ projects/django_book/learning_logs/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+
+from .models import Topic
 
 # Create your views here.
+
+def index(request):
+    """
+    The home page for learning_log app
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('data_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """
+    Show a single topic and all its entries
+
+    Args:
+        request (_type_): _description_
+        topic_id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
